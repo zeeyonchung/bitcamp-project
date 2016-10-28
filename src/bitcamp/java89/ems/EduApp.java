aca9b687c3ba3fa992a62eaac84d9dbe8bf867a7
Zeeyon Chung <zeeyon.c@gmail.com>
2016-10-28 11:17:16 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -7,7 +7,8 @@ public class EduApp {
 
 
   public static void main(String[] args) {
-    InstructorController.keyScan = keyScan;
+    //InstructorController.keyScan = keyScan;
+    InstructorController instructorController = new InstructorController(keyScan);
 
     System.out.println("비트캠프 관리 시스템에 오신 걸 환영합니다.");
 
@@ -17,9 +18,9 @@ public class EduApp {
       String command = keyScan.nextLine().toLowerCase();
 
       switch (command) {
-      case "add" : InstructorController.doAdd(); break;
-      case "list" : InstructorController.doList(); break;
-      case "view" : InstructorController.doView(); break;
+      case "add" : instructorController.doAdd(); break;
+      case "list" : instructorController.doList(); break;
+      case "view" : instructorController.doView(); break;
       case "quit" :
         System.out.println("Good bye!");
         break loop;
--- src/bitcamp/java89/ems/InstructorController.java
+++ src/bitcamp/java89/ems/InstructorController.java
@@ -3,13 +3,17 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class InstructorController {
-  static Instructor[] instrs = new Instructor[100];
-  static int length = 0;
-  static Scanner keyScan;
+  Instructor[] instrs = new Instructor[100];
+  int length = 0;
+  Scanner keyScan;
 
-  static void doList() {
-    for (int i = 0; i < length; i ++) {
-      Instructor instr = instrs[i];
+  public InstructorController(Scanner keyScan) {
+    this.keyScan = keyScan;
+  }
+
+  public void doList() {
+    for (int i = 0; i < this.length; i ++) {
+      Instructor instr = this.instrs[i];
       System.out.printf("%s, %s, %s, %s, %s, %s, %s, %s, %s\n",
         instr.name,
         instr.lectureName,
@@ -25,61 +29,61 @@ public class InstructorController {
   }
 
 
-  static void doAdd() {
-    while (length < instrs.length) {
+  void doAdd() {
+    while (length < this.instrs.length) {
       Instructor instr = new Instructor();
 
       System.out.print("이름(예:엄진영)? ");
-      instr.name = keyScan.nextLine();
+      instr.name = this.keyScan.nextLine();
 
       System.out.print("담당강의(예:JAVA 입문)? ");
-      instr.lectureName = keyScan.nextLine();
+      instr.lectureName = this.keyScan.nextLine();
 
       System.out.print("회사경력(예:롯데 전산실 2005-2006)? ");
-      instr.jobCareer = keyScan.nextLine();
+      instr.jobCareer = this.keyScan.nextLine();
 
       System.out.print("강의경력(예:비트캠프 자바 강의 2년)? ");
-      instr.lectureCareer = keyScan.nextLine();
+      instr.lectureCareer = this.keyScan.nextLine();
 
       System.out.print("저서(예:자바 시작)? ");
-      instr.book = keyScan.nextLine();
+      instr.book = this.keyScan.nextLine();
 
       System.out.print("학력(예:서울대 컴퓨터공학과)? ");
-      instr.school = keyScan.nextLine();
+      instr.school = this.keyScan.nextLine();
 
       System.out.print("강의평가(예:*****)? ");
-      instr.appraisal = keyScan.nextLine();
+      instr.appraisal = this.keyScan.nextLine();
 
       System.out.print("웹사이트(예:eom.com)? ");
-      instr.webSite = keyScan.nextLine();
+      instr.webSite = this.keyScan.nextLine();
 
       System.out.print("수상내역(예:## 공모전 입상)? ");
-      instr.prize = keyScan.nextLine();
+      instr.prize = this.keyScan.nextLine();
 
       instrs[length++] = instr;
 
       System.out.print("계속 입력하시겠습니까?(y/n)");
-      if (!keyScan.nextLine().equals("y")) {
+      if (!this.keyScan.nextLine().equals("y")) {
         break;
       }
     }
   }
 
 
-  static void doView() {
+  public void doView() {
     System.out.println("조회할 강사님의 성함을 입력하세요.");
     String instructorName = keyScan.nextLine().toLowerCase();
-    for (int i = 0; i < length; i++) {
-      if ( instrs[i].name.toLowerCase().equals(instructorName) ) {
-        System.out.printf("이름: %s\n", instrs[i].name);
-        System.out.printf("담당강의: %s\n", instrs[i].lectureName);
-        System.out.printf("회사경력: %s\n", instrs[i].jobCareer);
-        System.out.printf("강의경력: %s\n", instrs[i].lectureCareer);
-        System.out.printf("저서: %s\n", instrs[i].book);
-        System.out.printf("학력: %s\n", instrs[i].school);
-        System.out.printf("강의평가: %s\n", instrs[i].appraisal);
-        System.out.printf("웹사이트: %s\n", instrs[i].webSite);
-        System.out.printf("수상내역: %s\n", instrs[i].prize);
+    for (int i = 0; i < this.length; i++) {
+      if ( this.instrs[i].name.toLowerCase().equals(instructorName) ) {
+        System.out.printf("이름: %s\n", this.instrs[i].name);
+        System.out.printf("담당강의: %s\n", this.instrs[i].lectureName);
+        System.out.printf("회사경력: %s\n", this.instrs[i].jobCareer);
+        System.out.printf("강의경력: %s\n", this.instrs[i].lectureCareer);
+        System.out.printf("저서: %s\n", this.instrs[i].book);
+        System.out.printf("학력: %s\n", this.instrs[i].school);
+        System.out.printf("강의평가: %s\n", this.instrs[i].appraisal);
+        System.out.printf("웹사이트: %s\n", this.instrs[i].webSite);
+        System.out.printf("수상내역: %s\n", this.instrs[i].prize);
         break;
       }
     }
